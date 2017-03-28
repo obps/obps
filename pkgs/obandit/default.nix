b17669a07fed458598ec6ea59af071c923fa7a68
Valentin Reis <valentin.reis@gmail.com>
2017-03-28 16:46:01 +0200

    pkg change

--- pkgs/obandit/default.nix
+++ pkgs/obandit/default.nix
@@ -4,14 +4,14 @@ stdenv.mkDerivation rec {
 	name = "obandit-${version}";
 	version = "0.2.1";
 
-  src = ../../obandit;
+  #src = ../../obandit;
 
-	#src = fetchurl {
-		#url = "https://github.com/freuk/obandit/archive/v0.2.1-zenodo.tar.gz";
-		#sha256 = "1a8l4kvv811vy11gjj9nvz1px7s4zxrr3zi53zblvkibq56zzl7s";
-	#};
+  src = fetchurl {
+    url = "https://github.com/freuk/obandit/archive/v0.2.1-zenodo.tar.gz";
+    sha256 = "1a8l4kvv811vy11gjj9nvz1px7s4zxrr3zi53zblvkibq56zzl7s";
+  };
 
-	#unpackCmd = "tar xjf $src";
+  unpackCmd = "tar xjf $src";
 
 	buildInputs = [ ocaml findlib ocamlbuild topkg opam ];
 
