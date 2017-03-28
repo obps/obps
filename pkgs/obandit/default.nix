6cac5d16a1e6d91260ea641e46e219ddb8c7a0f0
Valentin Reis <valentin.reis@gmail.com>
2017-03-28 16:42:18 +0200

    expe sdsc reduce search space.

--- pkgs/obandit/default.nix
+++ pkgs/obandit/default.nix
@@ -3,12 +3,15 @@
 stdenv.mkDerivation rec {
 	name = "obandit-${version}";
 	version = "0.2.1";
-	src = fetchurl {
-		url = "https://github.com/freuk/obandit/archive/v0.2.1-zenodo.tar.gz";
-		sha256 = "1a8l4kvv811vy11gjj9nvz1px7s4zxrr3zi53zblvkibq56zzl7s";
-	};
 
-	unpackCmd = "tar xjf $src";
+  src = ../../obandit;
+
+	#src = fetchurl {
+		#url = "https://github.com/freuk/obandit/archive/v0.2.1-zenodo.tar.gz";
+		#sha256 = "1a8l4kvv811vy11gjj9nvz1px7s4zxrr3zi53zblvkibq56zzl7s";
+	#};
+
+	#unpackCmd = "tar xjf $src";
 
 	buildInputs = [ ocaml findlib ocamlbuild topkg opam ];
 
